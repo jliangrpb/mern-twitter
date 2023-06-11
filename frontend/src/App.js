19d341e4f94351840271b0eb0a7e32607aeb785f
jliangrpb <jeremy.liang612@gmail.com>
2023-06-11 16:51:39 -0400

    Complete auth p3-4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from './components/Routes/routes';
+import NavBar from './components/NavBar/navBar';
+
+import MainPage from './components/MainPage/mainPage';
+import LoginForm from './components/SessionForms/LoginForm';
+import SignupForm from './components/SessionForms/SignupForm';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
+    </>
   );
 }
 
